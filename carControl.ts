f80af415c7847c8c6731fdee2fff2e650a1c8c8f
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2021-01-03 14:50:09 +0900

    Update carControl.ts

--- carControl.ts
+++ carControl.ts
@@ -74,8 +74,11 @@ namespace carcotrol {
     function init() {
         if (initFlag == 0) {
             pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
-            if (pins.digitalReadPin(DigitalPin.P2) == 1) cartype = carType.Tinybit;
+            pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+            if (pins.digitalReadPin(DigitalPin.P15) == 1) cartype = carType.Porocar;
+            else if (pins.digitalReadPin(DigitalPin.P2) == 1) cartype = carType.Tinybit;
             else cartype = carType.Maqueen
+            pins.setPull(DigitalPin.P15, PinPullMode.PullNone)
             pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
             initFlag = 1;
         }
