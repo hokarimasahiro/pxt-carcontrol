18bf5610be56192fc372839e78e0ec24c1288d52
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2021-05-06 05:20:16 +0900

    更新 carControl.ts

--- carControl.ts
+++ carControl.ts
@@ -75,10 +75,20 @@ namespace carcotrol {
         if (initFlag == 0) {
             pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
             pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
-            if (pins.digitalReadPin(DigitalPin.P2) == 1 && pins.digitalReadPin(DigitalPin.P15) == 1) cartype = carType.Porocar;
-            else if (pins.digitalReadPin(DigitalPin.P2) == 1) cartype = carType.Tinybit;
-            else if (pins.digitalReadPin(DigitalPin.P15) == 1) cartype = carType.Maqueen;
-            else cartype = carType.Unknown
+            if (pins.digitalReadPin(DigitalPin.P2) == 1 && pins.digitalReadPin(DigitalPin.P15) == 1) {
+                cartype = carType.Porocar;
+                pins.setPull(DigitalPin.P1, PinPullMode.PullNone)
+                pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
+            }
+            else if (pins.digitalReadPin(DigitalPin.P2) == 1){
+                cartype = carType.Tinybit;
+            }
+            else if (pins.digitalReadPin(DigitalPin.P15) == 1){
+                cartype = carType.Maqueen;
+            }
+            else{
+                cartype = carType.Unknown
+            } 
             pins.setPull(DigitalPin.P15, PinPullMode.PullNone)
             pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
             initFlag = 1;
